82cb3d82155690c3a91f751e3e15c37561affa03
Luiz Fernando <89098587+LuizFernando0@users.noreply.github.com>
2021-08-31 18:53:59 -0300

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,13 @@
+Numero = int(input("Insira um número de 1 a 10 para poder ver a sua tabuada: "))
+print('{} x {} = {}'.format(Numero, 1, Numero*1))
+print('{} x {} = {}'.format(Numero, 2, Numero*2))
+print('{} x {} = {}'.format(Numero, 3, Numero*3))
+print('{} x {} = {}'.format(Numero, 4, Numero*4))
+print('{} x {} = {}'.format(Numero, 5, Numero*5))
+print('{} x {} = {}'.format(Numero, 6, Numero*6))
+print('{} x {} = {}'.format(Numero, 7, Numero*7))
+print('{} x {} = {}'.format(Numero, 8, Numero*8))
+print('{} x {} = {}'.format(Numero, 9, Numero*9))
+print('{} x {} = {}'.format(Numero, 10, Numero*10))
+
+print("Agora vai estudar menin@! Que Vergonha!")
